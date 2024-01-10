3272376f61ef8e3ceea62bd5c2bca6f3aa19ff69
jefmud <jefmud@gmail.com>
2024-01-10 14:24:32 -0500

    Update __init__.py
    make sure `user_services_cli` is made visible in the `__init__.py` script.

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
 # exports for bottle_simple_admin
-from .bottle_simple_admin import Admin, url_for, render_template
+from .bottle_simple_admin import Admin, url_for, render_template, user_services_cli
